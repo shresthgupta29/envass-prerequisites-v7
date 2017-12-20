575f251d0dff0ef2ba0353f888d2ce9832dc4f76
shresthgupta29 <33654121+shresthgupta29@users.noreply.github.com>
2017-12-20 13:10:31 +0530

    Update machines.py

--- machines.py
+++ machines.py
@@ -14,8 +14,6 @@ nfs='10.120.0.5' #internal ip of nfs
 db=["10.120.0.2","10.75.35.87"] #Internal IP,External IP
 
 zip_path = "" #path of the chefToolkit with the file itself Example : "/u02/app_files/PrimeeraXXXXXXXXXX.zip"  
-chef_path="/home/cloud-user/Primavera_Orchestration_toolkit_17.11.0.0"  #till Cheforchestration --> this folder should contain chef and testing folders
-
 
 m_list1 = [IDM_NodeA[0],IDM_NodeB[0],App01_NodeA[0],App01_NodeB[0],App02_NodeA[0],App02_NodeB[0]]
 m_list2  = [IDM_NodeA[0],App01_NodeA[0],App02_NodeA[0]]
